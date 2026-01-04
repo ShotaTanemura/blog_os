6e4b2fabdcd4f8c8047f1e6555056a9990ca88ac
ShotaTanemura <shota0711tane@gmail.com>
2026-01-04 17:02:14 +0900

    Allows testing in main.rs to use my own testing library

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
 use core::panic::PanicInfo;
+use blog_os::println;
 
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
 	println!("{}", info);
 	loop {}
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-	println!("Hello World{}", "!");
-	panic!("Some panic message");
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+	blog_os::test_panic_handler(info)
+}
 
-	loop{}
+#[test_case]
+fn trivial_assertion() {
+	assert_eq!(1, 1);
 }
