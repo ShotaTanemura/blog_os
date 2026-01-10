b8bfede2a2515841e72661f1789cec3015f53608
ShotaTanemura <shota0711tane@gmail.com>
2026-01-10 12:00:25 +0900

    feat: Allows double_fault_handler to handle double fault

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,5 +1,5 @@
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::println;
+use crate::{gdt, println};
 use lazy_static::lazy_static;
 
 
@@ -17,6 +17,10 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 #[test_case]
