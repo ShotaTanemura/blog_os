f20875b8de48a69032bfef5a3defa3c4219e5626
ShotaTanemura <shota0711tane@gmail.com>
2026-01-11 23:18:25 +0900

    feat: Allows a kernel to introduece memory module

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,24 @@ use core::panic::PanicInfo;
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::memory;
+    use blog_os::memory::BootInfoFrameAllocator;
+    use x86_64::VirtAddr;
+    // use x86_64::{VirtAddr, structures::paging::Page};
+
     println!("Hello World{}", "!");
 
     blog_os::init();
 
-    use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut _mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut _frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!(
-        "Level 4 page table at: {:?}",
-        level_4_page_table.start_address()
-    );
+    // let page = Page::containing_address(VirtAddr::new(0));
+    // memory::crate_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
